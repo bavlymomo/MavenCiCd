b21f8f412e85366dd9c52dc6f451a15cff3bc173
bavlymomo <bavlymomtaz10@gmail.com>
2025-10-20 04:28:52 +0300

    Added Employee class

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Employee {
+    int salary ;
+    String Name;
+
+    public Employee(int salary , String Name) {
+        this.Name = Name;
+        this.salary = salary;
+    }
+}
